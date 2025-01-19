5ea0769a81e05f4ef4c86fcc216dd0bf94ab910c
Guilherme Tavares <joseserafimtavares89@gmail.com>
2025-01-19 14:49:08 +0000

    GetAll-Reservations Feito
    E testado

---
+++ RestaurantReservationAPI/Models/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using RestaurantReservationAPI.Models;
+using System;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            // Carregar a variável de ambiente
+            var connectionString = Environment.GetEnvironmentVariable("DefaultConnection");
+            if (string.IsNullOrWhiteSpace(connectionString))
+            {
+                throw new InvalidOperationException("Connection string 'DefaultConnection' not found in environment variables.");
+            }
+
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+
+    public DbSet<Reservation> Reservations { get; set; }
+}
