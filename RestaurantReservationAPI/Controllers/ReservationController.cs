9eed95c55dd66d496c4dbcd41ebff0db9beba1e7
Camila Giacomelli <camilikagiacomelli@gmail.com>
2025-01-21 19:28:55 +0000

    UpdateReservations e GetReservationsByDate feito

--- RestaurantReservationAPI/Controllers/ReservationController.cs
+++ RestaurantReservationAPI/Controllers/ReservationController.cs
@@ -20,5 +20,42 @@ namespace RestaurantReservationAPI.Controllers
         {
             return Ok(_context.Reservations.ToArray());
         }
+
+        // PUT: api/reservation/{id}
+        [HttpPut("{id}")]
+        public IActionResult UpdateReservation(int id, [FromBody] Reservation updatedReservation)
+        {
+            Reservation reservation = _context.Reservations.FirstOrDefault(res => res.Id == id);
+            if (reservation == null)
+            {
+                return NotFound();
+            }
+
+            reservation.CustomerName = updatedReservation.CustomerName;
+            reservation.ReservationDate = updatedReservation.ReservationDate;
+            reservation.ReservationTime = updatedReservation.ReservationTime;
+            reservation.TableNumber = updatedReservation.TableNumber;
+            reservation.NumberOfPeople = updatedReservation.NumberOfPeople;
+
+            _context.SaveChanges();
+            return Ok(reservation);
+        }
+
+        // GET: api/reservations?date={date}
+        [HttpGet]
+        public IActionResult GetReservationsByDate([FromQuery] DateTime date)
+        {
+            var reservations = _context.Reservations
+                .Where(reservation => reservation.ReservationDate.Date == date.Date)
+                .ToList();
+
+            if (!reservations.Any())
+            {
+                return NotFound("No reservations found for the specified date.");
+            }
+
+            return Ok(reservations);
+        }
+
     }
 }
